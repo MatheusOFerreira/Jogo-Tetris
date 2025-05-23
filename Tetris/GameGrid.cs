1d1975319af64958bcb9e22ddc42d38f1318ff11
MatheusOFerreira <matheus.oferreira@hotmail.com>
2025-05-23 14:38:19 -0300

    Finalizacao da classe GameGrid

--- Tetris/GameGrid.cs
+++ Tetris/GameGrid.cs
@@ -53,9 +53,9 @@
         //Método parecido com o superior, este verifica se a linha está vazia
         public bool IsRowEmpty(int r)
         {
-            for (int c = 0; c< Columns; c++)
+            for (int c = 0; c < Columns; c++)
             {
-                if(grid[r, c] != 0)
+                if (grid[r, c] != 0)
                 {
                     return false;
                 }
@@ -63,6 +63,48 @@
             return true;
         }
 
+        //Método que limpa a linha quando ela está completa
+        private void ClearRow(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                grid[r, c] = 0;
+            }
+        }
 
+        //Método que move a linha para baixo
+        //Recebe o r(linha que será mexida) e a quantidade de linhas que a linha R será movida para baixo
+        private void MoveRowDown(int r, int numRows)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                //Move a célula atual para a posição r + linhas abaixo
+                grid[r + numRows, c] = grid[r, c];
+                //Move e apaga, pra mostrar que tá vazia
+                grid[r, c] = 0;
+            }
+        }
+
+        public int ClearFullRows()
+        {
+            //A variável limpa vai começar em 0 e será movida da linha inferior para a superior
+            int cleared = 0;
+
+            for (int r = Rows - 1; r >= 0; r--)
+            {
+                //Vamos verificar se a linha atual está cheia e se estiver, irá ser limpada e incrementada
+                if (IsRowFull(r))
+                {
+                    ClearRow(r);
+                    cleared++;
+                }
+                //Caso contrário, se limpo for maior que zero, vamos mover a linha atual para baixo pelo número de linhas limpas
+                else if (cleared > 0)
+                {
+                    MoveRowDown(r, cleared);
+                }
+            }
+            return cleared;
+        }
     }
 }
