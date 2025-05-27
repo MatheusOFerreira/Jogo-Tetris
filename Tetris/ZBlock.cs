86ea39db32e0470962e89a54301a250c107f170e
MatheusOFerreira <matheus.oferreira@hotmail.com>
2025-05-27 14:46:49 -0300

    Implementacao das classes LBlock, SBlock, TBlock e ZBlock

---
+++ Tetris/ZBlock.cs
@@ -0,0 +1,20 @@
+﻿namespace Tetris
+{
+    public class ZBlock : Block
+    {
+        //Classe que representa a peça I do jogo, tiles contém todas rotações possíveis
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] { new Position(0,0), new Position(0,1), new Position(1,1), new Position(1,2) },
+            new Position[] { new Position(0,2), new Position(1,1), new Position(1,2), new Position(2,1) },
+            new Position[] { new Position(1,0), new Position(1,1), new Position(2,1), new Position(2,2) },
+            new Position[] { new Position(0,1), new Position(1,0), new Position(1,1), new Position(2,0) }
+        };
+
+        //Id UNICO para a peça I
+        public override int Id => 7;
+        //Definição da posição inicial da peça no jogo
+        protected override Position StartOffset => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+    }
+}
