9af15be2071cdfb1e591866d42bd26f5178eab87
MatheusOFerreira <matheus.oferreira@hotmail.com>
2025-05-27 14:56:46 -0300

    Implementacao da classe BlockQueue, que e responsavel por dizer qual e a proxima peca do jogo

---
+++ Tetris/BlockQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Tetris
+{
+    //Classe responsável por dizer quem é a próxima peça no jogo
+    //Contém uma matriz de blocos com uma instrancia das 7 classes de blocos 
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock()
+        };
+
+        //Objeto aleatório 
+        private readonly Random random = new Random();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+        {
+            NextBlock = RandomBlock();
+        }
+
+        //Método que retorna um bloco aleatório
+        private Block RandomBlock()
+        {
+            return blocks[random.Next(blocks.Length)];
+        }
+
+        //Método que retorna o próximo bloco e atualiza a propriedade
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            } while (block.Id == NextBlock.Id); //Para não retornar o mesmo bloco duas vezes seguidas
+
+            return block;
+        }
+    }
+}
