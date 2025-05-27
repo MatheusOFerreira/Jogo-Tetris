8e88ab93969b952eaf3314445a143c01e6646781
MatheusOFerreira <matheus.oferreira@hotmail.com>
2025-05-27 10:31:55 -0300

    Inicio da implementacao da classe Block

---
+++ Tetris/Block.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic; 
+
+namespace Tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; } 
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        //Método que retorna as posições da grade ocupadas pelo bloco fatorando na rotação atual e deslocamento
+        public IEnumerable<Position> TilePositions()
+        {
+            //O método vai fazer um loop sobre as posições dos bloco no estrado de rotação atual e adiciona o deslocamento da linha e o deslocamento da coluna
+            foreach(Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        //Este método gira o bloco 90 graus no sentido horário, isso será incrementado oe stado de rotação atual voltando para zero se estiver no estado final
+
+    }
+}
